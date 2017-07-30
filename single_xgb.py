c88ea88c7e1293e2d16a70b2f5dfba2e70111604
Feixiang Ni <2643104428@qq.com>
2017-07-30 15:24:53 +0800

    Delete single_xgb.py

--- single_xgb.py
+++
@@ -1,9 +0,0 @@
-import xgboost as xgb
-import pandas as pd
-import numpy as np
-
-
-def my_xgb_cv(x_train,y_train, NFOLDS, xgb_params):
-	dtrain = xgb.DMatrix(data=x_train, label=y_train)
-	bst = xgb.cv(xgb_params, dtrain, 10000, NFOLDS,early_stopping_rounds=60,verbose_eval= 50)
-	return bst
